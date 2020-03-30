ffe64ae54a3c3575a2bb02816cdd196afe0a82ba
Alexandra <stannida@gmail.com>
2020-03-30 12:51:26 +0200

    R notebooks for gender package

---
+++ install.R
@@ -0,0 +1,5 @@
+install.packages(c("gender", "genderdata"),
+                 repos = "http://packages.ropensci.org",
+                 type = "source")
+install.packages("shiny")
+install.packages("shinydashboard")
