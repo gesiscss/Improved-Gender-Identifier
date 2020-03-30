4301e6358888e90d590aec57f61c2edffd8612c3
Aleksandra Stankevich <stannida@gmail.com>
2020-03-30 17:16:20 +0300

    deleting r file for master

--- install.R
+++
@@ -1,5 +0,0 @@
-install.packages(c("gender", "genderdata"),
-                 repos = "http://packages.ropensci.org",
-                 type = "source")
-install.packages("shiny")
-install.packages("shinydashboard")
